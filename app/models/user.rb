fc5f2f85dc2aef455ea7fff5ac448b53d78b3acd
Goasd <jonne.kari@gmail.com>
2014-05-22 14:12:19 +0300

    username uniikiksi

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :yearOfBirth, numericality: { greater_than_or_equal_to: 1900,
                                           less_than_or_equal_to: ->(_) { Time.now.year},
                                only_integer: true }
+
+  validates :username, uniqueness: true
 end
