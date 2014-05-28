34547d82208c71ec1c96338ef947efbf64496eff
Kaikkonen Arto M <artokaik@da1-cs-b221-27.helsinki.fi>
2014-05-28 14:02:22 +0300

    list-table, active and training columns

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,6 +34,9 @@ class ListsController < ApplicationController
     csvfile = params[:file]
     @list = List.new(list_params)
 
+    @list.active = true
+    @list.training = false
+
     @list.filename = csvfile.original_filename
     @list.save()
 
