90c0f54ad76a0f2c6c62117cca99a562b71823a1
Kaikkonen Arto M <artokaik@da1-cs-b221-27.helsinki.fi>
2014-06-05 09:36:16 +0300

    testcases controller

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -1,78 +1,78 @@
-# class TestcasesController < ApplicationController
-#   before_action :set_testcase, only: [:show, :edit, :update, :destroy]
-#   skip_before_filter  :verify_authenticity_token
-#
-#   before_filter :cors_preflight_check
-#   after_filter :cors_set_access_control_headers
-#
-#   # GET /testcases
-#   # GET /testcases.json
-#   def index
-#     @testcases = Testcase.all
-#   end
-#
-#   # GET /testcases/1
-#   # GET /testcases/1.json
-#   def show
-#   end
-#
-#   # GET /testcases/new
-#   def new
-#     @testcase = Testcase.new
-#   end
-#
-#   # GET /testcases/1/edit
-#   def edit
-#   end
-#
-#   # POST /testcases
-#   # POST /testcases.json
-#   def create
-#     @testcase = Testcase.new(testcase_params)
-#
-#     respond_to do |format|
-#       if @testcase.save
-#         format.html { redirect_to @testcase, notice: 'Testcase was successfully created.' }
-#         format.json { render action: 'show', status: :created, location: @testcase }
-#       else
-#         format.html { render action: 'new' }
-#         format.json { render json: @testcase.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-#
-#   # PATCH/PUT /testcases/1
-#   # PATCH/PUT /testcases/1.json
-#   def update
-#     respond_to do |format|
-#       if @testcase.update(testcase_params)
-#         format.html { redirect_to @testcase, notice: 'Testcase was successfully updated.' }
-#         format.json { head :no_content }
-#       else
-#         format.html { render action: 'edit' }
-#         format.json { render json: @testcase.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-#
-#   # DELETE /testcases/1
-#   # DELETE /testcases/1.json
-#   def destroy
-#     @testcase.destroy
-#     respond_to do |format|
-#       format.html { redirect_to testcases_url }
-#       format.json { head :no_content }
-#     end
-#   end
-#
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_testcase
-#       @testcase = Testcase.find(params[:id])
-#     end
-#
-#     # Never trust parameters from the scary internet, only allow the white list through.
-#     def testcase_params
-#       params.require(:testcase).permit(:user_id, :list_id, :training, :finished)
-#     end
-# end
+class TestcasesController < ApplicationController
+  before_action :set_testcase, only: [:show, :edit, :update, :destroy]
+  skip_before_filter  :verify_authenticity_token
+
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  # GET /testcases
+  # GET /testcases.json
+  def index
+    @testcases = Testcase.all
+  end
+
+  # GET /testcases/1
+  # GET /testcases/1.json
+  def show
+  end
+
+  # GET /testcases/new
+  def new
+    @testcase = Testcase.new
+  end
+
+  # GET /testcases/1/edit
+  def edit
+  end
+
+  # POST /testcases
+  # POST /testcases.json
+  def create
+    @testcase = Testcase.new(testcase_params)
+
+    respond_to do |format|
+      if @testcase.save
+        format.html { redirect_to @testcase, notice: 'Testcase was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @testcase }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @testcase.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /testcases/1
+  # PATCH/PUT /testcases/1.json
+  def update
+    respond_to do |format|
+      if @testcase.update(testcase_params)
+        format.html { redirect_to @testcase, notice: 'Testcase was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @testcase.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /testcases/1
+  # DELETE /testcases/1.json
+  def destroy
+    @testcase.destroy
+    respond_to do |format|
+      format.html { redirect_to testcases_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_testcase
+      @testcase = Testcase.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def testcase_params
+      params.require(:testcase).permit(:user_id, :list_id, :training, :finished)
+    end
+end
