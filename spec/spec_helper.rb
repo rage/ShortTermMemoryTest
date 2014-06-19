e31cd0d201699a1523542bbd858559da4dab4641
Goasd <jonne.kari@gmail.com>
2014-06-19 18:49:49 +0300

    coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
- 
+ require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
-require "codeclimate-test-reporter"
 require 'capybara/rspec'
 require 'simplecov'
-SimpleCov.start('rails')  
-
+SimpleCov.start('rails')   
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
