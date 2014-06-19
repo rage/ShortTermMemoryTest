c4bffd9510c0e8af5fe857d3ca76f24696f016e9
Goasd <jonne.kari@gmail.com>
2014-06-19 19:03:34 +0300

    coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
- require "codeclimate-test-reporter"
+require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require 'capybara/rspec'
@@ -9,6 +9,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
